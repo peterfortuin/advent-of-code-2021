d49a02d493b934597761aecc7aa590912f97885a
Peter Fortuin <peter.fortuin@gmail.com>
2021-12-01 10:23:36 +0100

    Puzzle 1.1

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,9 +1,6 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.map { it.toInt() }.fold(Pair(Int.MAX_VALUE, 0)) { acc, next ->
-            val numberOfIncreases = if (next > acc.first) acc.second + 1 else acc.second
-            Pair(next, numberOfIncreases)
-        }.second
+        return input.size
     }
 
     fun part2(input: List<String>): Int {
@@ -12,7 +9,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 7)
+    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))

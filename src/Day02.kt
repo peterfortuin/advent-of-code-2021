b5fb6256a3b16587438d0bf629f0e2bd6bea4be0
Peter Fortuin <peter.fortuin@gmail.com>
2021-12-02 07:50:10 +0100

    Puzzle 2.1

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,6 +1,48 @@
+class SubmarinePosition {
+    var horizontal = 0
+    var depth = 0
+}
+
+abstract class SubmarineCommand() {
+    abstract fun execute(position: SubmarinePosition)
+}
+
+class ForwardCommand(private val amount: Int) : SubmarineCommand() {
+    override fun execute(position: SubmarinePosition) {
+        position.horizontal += amount
+    }
+}
+
+class DownCommand(private val amount: Int) : SubmarineCommand() {
+    override fun execute(position: SubmarinePosition) {
+        position.depth += amount
+    }
+}
+
+class UpCommand(private val amount: Int) : SubmarineCommand() {
+    override fun execute(position: SubmarinePosition) {
+        position.depth -= amount
+    }
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val position = input
+            .map { line -> line.split(" ") }
+            .map { textCommand ->
+                when (textCommand[0]) {
+                    "forward" -> ForwardCommand(textCommand[1].toInt())
+                    "down" -> DownCommand(textCommand[1].toInt())
+                    "up" -> UpCommand(textCommand[1].toInt())
+                    else -> throw IllegalStateException("Unknown command")
+                }
+            }
+            .fold(SubmarinePosition()) { position, command ->
+                command.execute(position)
+                position
+            }
+
+        return position.depth * position.horizontal
     }
 
     fun part2(input: List<String>): Int {
@@ -8,10 +50,10 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 150)
 
-    val input = readInput("Day01")
+    val input = readInput("Day02")
     println(part1(input))
     println(part2(input))
 }

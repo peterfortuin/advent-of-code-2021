8ced376d3f5e1c5af0050667b1473d8a88052578
Peter Fortuin <peter.fortuin@gmail.com>
2021-12-06 22:30:07 +0100

    Puzzle 5.2

--- src/Day05.kt
+++ src/Day05.kt
@@ -6,7 +6,7 @@ data class Line(
 )
 
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun count2OrLarger(input: List<String>, doDiagonals: Boolean = false): Int {
         val regex = """([0-9]*),([0-9]*) -> ([0-9]*),([0-9]*)""".toRegex()
 
         val numberOf2OrLarger = input
@@ -16,14 +16,30 @@ fun main() {
             }.map { groupValues ->
                 Line(groupValues[1].toInt(), groupValues[2].toInt(), groupValues[3].toInt(), groupValues[4].toInt())
             }.fold(Array(1000) { IntArray(1000) }) { acc, line ->
-                if (line.x1 == line.x2) {
-                    (minOf(line.y1, line.y2)..maxOf(line.y1, line.y2)).forEach { y ->
-                        acc[y][line.x1] += 1
+                val xRange = minOf(line.x1, line.x2)..maxOf(line.x1, line.x2)
+                val yRange = minOf(line.y1, line.y2)..maxOf(line.y1, line.y2)
+
+                when {
+                    line.x1 == line.x2 -> {
+                        yRange.forEach { y ->
+                            acc[y][line.x1] += 1
+                        }
                     }
-                }
-                if (line.y1 == line.y2) {
-                    (minOf(line.x1, line.x2)..maxOf(line.x1, line.x2)).forEach { x ->
-                        acc[line.y1][x] += 1
+                    line.y1 == line.y2 -> {
+                        xRange.forEach { x ->
+                            acc[line.y1][x] += 1
+                        }
+                    }
+                    doDiagonals -> {
+                        val xIterator = (line.x1 toward line.x2).iterator()
+                        val yIterator = (line.y1 toward line.y2).iterator()
+
+                        while (xIterator.hasNext() && yIterator.hasNext()) {
+                            val x = xIterator.next()
+                            val y = yIterator.next()
+
+                            acc[y][x] += 1
+                        }
                     }
                 }
                 acc
@@ -40,13 +56,18 @@ fun main() {
         return numberOf2OrLarger
     }
 
+    fun part1(input: List<String>): Int {
+        return count2OrLarger(input)
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        return count2OrLarger(input, true)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
     check(part1(testInput) == 5)
+    check(part2(testInput) == 12)
 
     val input = readInput("Day05")
     println(part1(input))
--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,8 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+infix fun Int.toward(to: Int): IntProgression {
+    val step = if (this > to) -1 else 1
+    return IntProgression.fromClosedRange(this, to, step)
+}

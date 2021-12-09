4443b6f57516a1d7d3c0080ce8b989f5604c5fcd
Peter Fortuin <peter.fortuin@gmail.com>
2021-12-09 17:16:26 +0100

    Puzzle 7.2

--- src/Day07.kt
+++ src/Day07.kt
@@ -14,12 +14,31 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val originalPositions = input.first().split(",").map { it.toInt() }
+        val maxPosition = originalPositions.maxOrNull() ?: 0
+
+        val bestHorizontalPositionAndFuel = (0..maxPosition).fold(Pair(-1, Int.MAX_VALUE)) { acc, currentPosition ->
+            val fuelUsedForThisPosition = originalPositions.fold(0) { acc, next ->
+                val distance = (currentPosition - next).absoluteValue
+                val fuelNeeded = acc + distance * (distance + 1) / 2
+
+                fuelNeeded
+            }
+
+            if (fuelUsedForThisPosition < acc.second) {
+                Pair(currentPosition, fuelUsedForThisPosition)
+            } else {
+                acc
+            }
+        }
+
+        return bestHorizontalPositionAndFuel.second
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_test")
     check(part1(testInput) == 37)
+    check(part2(testInput) == 168)
 
     val input = readInput("Day07")
     println(part1(input))

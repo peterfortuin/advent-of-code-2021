0a18278d0956acc3048d183821540f33f270cafb
Peter Fortuin <peter.fortuin@gmail.com>
2021-12-09 21:42:30 +0100

    Puzzle 8.1

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,6 +1,17 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val count = input
+            .map { line ->
+                line.split("|")[1].trim()
+            }
+            .map { values ->
+                values.split(" ")
+            }
+            .flatten()
+            .count { value ->
+                value.length == 2 || value.length == 4 || value.length == 3 || value.length == 7
+            }
+        return count
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +20,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day08_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 26)
 
     val input = readInput("Day08")
     println(part1(input))

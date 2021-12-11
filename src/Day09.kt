790c4f110e0ef4096a403b536517a4a040753ae6
Peter Fortuin <peter.fortuin@gmail.com>
2021-12-11 11:57:31 +0100

    Puzzle 9.1

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,6 +1,27 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val raster = input
+            .map { line ->
+                line.toList().map { numberChar ->
+                    numberChar.toString().toInt()
+                }
+            }
+
+        val totalRiskLevel = raster.foldIndexed(0) { row, riskLevel, line ->
+            line.foldIndexed(riskLevel) { column, riskLvl, item ->
+                val left = if (column > 0) raster[row][column - 1] else 10
+                val right = if (column < line.size - 1) raster[row][column + 1] else 10
+                val top = if (row > 0) raster[row - 1][column] else 10
+                val bottom = if (row < raster.size - 1) raster[row + 1][column] else 10
+
+                if (left > item && right > item && top > item && bottom > item)
+                    riskLvl + item + 1
+                else
+                    riskLvl
+            }
+        }
+
+        return totalRiskLevel
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +30,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day09_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 15)
 
     val input = readInput("Day09")
     println(part1(input))

b213a6ad6fff498b9637ecb836865ef0b3298f37
Peter Fortuin <peter.fortuin@gmail.com>
2021-12-13 19:34:28 +0100

    Puzzle 10.1

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,6 +1,45 @@
 fun main() {
+    val symbols = mapOf(
+        '(' to ')',
+        '[' to ']',
+        '{' to '}',
+        '<' to '>',
+    )
+
+    fun findCorrupted(line: String): Int {
+        var lineToParse = line
+
+        while (lineToParse.isNotEmpty()) {
+            var itemRemoved = false
+            (0..lineToParse.length - 2).forEach { i ->
+                if (itemRemoved) return@forEach
+
+                if (symbols.containsValue(lineToParse[i + 1]) && lineToParse[i + 1] != symbols[lineToParse[i]]) {
+                    return when (lineToParse[i + 1]) {
+                        ')' -> 3
+                        ']' -> 57
+                        '}' -> 1197
+                        '>' -> 25137
+                        else -> throw RuntimeException("Not possible")
+                    }
+                }
+
+                if (lineToParse[i + 1] == symbols[lineToParse[i]]) {
+                    lineToParse = lineToParse.removeRange(i, i + 2)
+                    itemRemoved = true
+                }
+            }
+
+            if (!itemRemoved) {
+                break
+            }
+        }
+
+        return 0
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { line -> findCorrupted(line) }.sum()
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +48,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day10_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 26397)
 
     val input = readInput("Day10")
     println(part1(input))

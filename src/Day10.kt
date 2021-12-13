38f732d0ed1229d494057f3d595d4566458dadd3
Peter Fortuin <peter.fortuin@gmail.com>
2021-12-13 22:30:25 +0100

    Puzzle 10.2

--- src/Day10.kt
+++ src/Day10.kt
@@ -42,13 +42,59 @@ fun main() {
         return input.map { line -> findCorrupted(line) }.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val scores = input
+            .asSequence()
+            .mapNotNull { line ->
+                if (findCorrupted(line) == 0)
+                    line
+                else
+                    null
+            }
+            .map { line ->
+                line.toCharArray()
+            }
+            .map { line ->
+                line.fold(Stack<Char>()) { stack, c ->
+                    if (symbols.keys.contains(c)) {
+                        stack.push(c)
+                    }
+
+                    // stack.peek() != null && symbols.containsKey(stack.peek()) && symbols[stack.peek()]
+                    if (symbols.values.contains(c)) {
+                        val symbolToClose = stack.peek()
+                        if (symbolToClose != null && symbols[symbolToClose] == c) {
+                            stack.pop()
+                        }
+                    }
+
+                    stack
+                }
+            }
+            .map { stack -> stack.reversed() }
+            .map { stack ->
+                stack.toList().fold(0L) { acc, c ->
+                    val points = when (c) {
+                        '(' -> 1
+                        '[' -> 2
+                        '{' -> 3
+                        '<' -> 4
+                        else -> throw RuntimeException("Not possible")
+                    }
+
+                    acc * 5 + points
+                }
+            }
+            .toList()
+            .sorted()
+
+        return scores[(scores.size - 1) / 2]
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day10_test")
     check(part1(testInput) == 26397)
+    check(part2(testInput) == 288957L)
 
     val input = readInput("Day10")
     println(part1(input))
